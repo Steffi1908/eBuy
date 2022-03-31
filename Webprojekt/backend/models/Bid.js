8be75a7ce39f180f52ef00960003b6a8386d36a2
Stefanie von Siegroth <steffi-vs@web.de>
2022-03-31 17:32:09 +0200

    new

---
+++ Webprojekt/backend/models/Bid.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+//Schema erstellen - damit wird festgelegt, wie ein Gebot aussieht
+
+
+const BidSchema = mongoose.Schema({
+    articleID: {
+        type: String,
+        required: true 
+    },
+    userID: {
+        type: String,
+        required: true 
+    },
+    price: {
+       type: Number,
+       required: true
+   }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Bid', BidSchema);         //dies sieht man dann in der Datenbank als "Bid"
